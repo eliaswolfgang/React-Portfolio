cb191275461d72ed3a51321c1a20d5929bd58c1a
Ian E Von Fange <wolfgangvonfange@gmail.com>
2023-01-29 07:37:14 -0600

    Updated portfolio

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,51 +1,51 @@
 export const projects = [
-    {
-        name: "fanPagr",
-        description: "A customizable fan page generator built using Materialize and jQuery.",
-        website: "https://cmoss703.github.io/fanPagR/",
-        gitHub: "https://github.com/cmoss703/fanPagR"
-    },
-    {
-        name: "Weather Dashboard",
-        description: "A basic weather app built using Bootstrap, jQuery, and the Open Weather Map API.",
-        website: "https://eliaswolfgang.github.io/Weather-Dashboard/",
-        gitHub: "https://github.com/eliaswolfgang/Weather-Dashboard"
-    },
-    {
-        name: "Node Note Taker",
-        description: "A simple note-taking application built using Express and Node.js.",
-        website: "https://agile-caverns-54810.herokuapp.com/notes",
-        gitHub: "https://github.com/eliaswolfgang/Node-Note-Taker"
-    },
-    {
-        name: "Progressive Budget App",
-        description: "A progressive web app for budgeting which works offline - if you lose your internet connection, no worries!",
-        website: "https://eliaswolfgang-budgeter.herokuapp.com/",
-        gitHub: "https://github.com/eliaswolfgang/Progressive-Budget-App"
-    },
-    {
-        name: "Employee Directory",
-        description: "An app for sorting and filtering employee information (first and last names and email addresses) retrieved from the Random User API.",
-        website: "https://eliaswolfgang.github.io/Employee-Directory/",
-        gitHub: "https://github.com/eliaswolfgang/Employee-Directory"
-    },
-    {
-        name: "Routinify",
-        description: "A full-stack task management app (Express, Handlebars, Sequelize).",
-        website: "https://routinify.herokuapp.com/",
-        gitHub: "https://github.com/Keaton-Brewster/project-2"
-    },
-    {
-        name: "Vision Boarder",
-        description: "Single-page app allowing users to create vision boards, spurring inspiration and motivation for their next project. (Sequelize, Express, React)",
-        website: "https://vision-board-learning-place.herokuapp.com",
-        gitHub: "https://github.com/maggiemaywilder/vision-board"
-    },
-    {
-        name: "Password Generator",
-        description: "Dynamic password generator application that randomly generates a password based on a length specified by the user. Through a series of prompts, users are given the option to include lowercase letters, uppercase letters, numbers, and special characters in their passwords.",
-        website: "https://eliaswolfgang.github.io/Password-Generator/",
-        gitHub: "https://github.com/eliaswolfgang/Password-Generator"
-    }
-
-]
+  {
+    name: 'fanPagr',
+    description:
+      'A customizable fan page generator built using Materialize and jQuery.',
+    website: 'https://cmoss703.github.io/fanPagR/',
+    gitHub: 'https://github.com/cmoss703/fanPagR',
+  },
+  {
+    name: 'Weather Dashboard',
+    description:
+      'A basic weather app built using Bootstrap, jQuery, and the Open Weather Map API.',
+    website: 'https://eliaswolfgang.github.io/Weather-Dashboard/',
+    gitHub: 'https://github.com/eliaswolfgang/Weather-Dashboard',
+  },
+  {
+    name: 'Node Note Taker',
+    description:
+      'A simple note-taking application built using Express and Node.js.',
+    website: 'https://agile-caverns-54810.herokuapp.com/notes',
+    gitHub: 'https://github.com/eliaswolfgang/Node-Note-Taker',
+  },
+  {
+    name: 'Progressive Budget App',
+    description:
+      'A progressive web app for budgeting which works offline - if you lose your internet connection, no worries!',
+    website: 'https://eliaswolfgang-budgeter.herokuapp.com/',
+    gitHub: 'https://github.com/eliaswolfgang/Progressive-Budget-App',
+  },
+  {
+    name: 'Employee Directory',
+    description:
+      'An app for sorting and filtering employee information (first and last names and email addresses) retrieved from the Random User API.',
+    website: 'https://eliaswolfgang.github.io/Employee-Directory/',
+    gitHub: 'https://github.com/eliaswolfgang/Employee-Directory',
+  },
+  {
+    name: 'Just Tech News',
+    description:
+      'A full-stack social media app for developers to share links (React, Sequelize, Material UI).',
+    website: 'https://ivf-tech-news.herokuapp.com/',
+    gitHub: 'https://github.com/eliaswolfgang/just-tech-news',
+  },
+  {
+    name: 'Password Generator',
+    description:
+      'Dynamic password generator application that randomly generates a password based on a length specified by the user. Through a series of prompts, users are given the option to include lowercase letters, uppercase letters, numbers, and special characters in their passwords.',
+    website: 'https://eliaswolfgang.github.io/Password-Generator/',
+    gitHub: 'https://github.com/eliaswolfgang/Password-Generator',
+  },
+];
